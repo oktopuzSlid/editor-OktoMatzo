ab61d44633d3595680768216da025e2ebe600aa0
Okto <55849643+oktopuzSlid@users.noreply.github.com>
2025-06-15 03:21:43 -0400

    opcionesdeInput

---
+++ detector.py
@@ -0,0 +1,11 @@
+
+from ultralytics import YOLO
+from config import MODEL_PATH, CONFIDENCE_THRESHOLD
+
+class YoloDetector:
+    def __init__(self):
+        self.model = YOLO(MODEL_PATH)
+
+    def detect(self, frame):
+        results = self.model(frame, conf=CONFIDENCE_THRESHOLD)
+        return results[0]
---
+++ modelo.py
@@ -0,0 +1,7 @@
+INPUT_TYPE = 'webcam'  
+VIDEO_PATH = 'ruta/a/tu/video.mp4'
+IMAGE_PATH = 'ruta/a/tu/imagen.jpg'
+
+
+MODEL_PATH = "yolov8n.pt"
+CONFIDENCE_THRESHOLD = 0.5
